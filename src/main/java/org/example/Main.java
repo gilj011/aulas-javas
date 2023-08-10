68e743727734bca29807fec577ef156b73497178
gilj011 <gilberto.camposjr@sptech.school>
2023-08-10 11:08:08 -0300

    newcomit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer idade = 18;
+        String nome = "Gilberto";
+
+        System.out.printf("Olá " + nome + " Você tem:\n"+ idade + " anos");
+    }
+}
