9a15d1e2d219dc0648d3b5c7175167d69dd61557
gilj011 <gilberto.camposjr@sptech.school>
2023-08-10 11:08:08 -0300

    newcomit

---
+++ src/main/java/org/example/TesteUm.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Enumeration;
+
+public class TesteUm{
+
+    public static void main(String[] args) {
+
+        Integer n1 = 11;
+        Integer n2 = 10;
+        /*
+        Double aleatorio = Math.random();
+        System.out.println("Soma: " + (n1+n2) + " " +aleatorio);
+        */
+
+        if (n1 == 10) {
+            System.out.println("é engual a 10");
+        }else{
+            System.out.println("nao é engual");
+        }
+        Boolean isIgual = n1 == n2;
+                while (isIgual){
+
+                }
+    }
+}
